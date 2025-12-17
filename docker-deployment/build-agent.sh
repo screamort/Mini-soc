71ba1791dce1914a6aae658ec54ff840ded9705a
screamort <mickael.cayroche@gmail.com>
2025-12-17 10:37:47 +0100

    Remove unused Wazuh/OpenSearch files and directories

--- docker-deployment/build-agent.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build Wazuh agent image
-docker build -f Dockerfile.wazuh-agent -t mini-soc-wazuh-agent:latest .
-
-echo "Wazuh agent image built successfully!"
-echo "You can now add this service to docker-compose.yml"
--- docker-deployment/copy-rules.sh
+++
@@ -1,2 +0,0 @@
-# Copy custom Wazuh rules to this location
-cp ../02-siem-configs/wazuh/custom-rules.xml ./custom-rules.xml
